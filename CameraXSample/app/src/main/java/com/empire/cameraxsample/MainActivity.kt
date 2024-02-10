200bda4cb710b464e2029cf93e178a03312241ba
Henok <henokmicrometre@gmail.com>
2024-02-10 17:10:08 +0000

    added camerax

--- CameraXSample/app/src/main/java/com/empire/cameraxsample/MainActivity.kt
+++ CameraXSample/app/src/main/java/com/empire/cameraxsample/MainActivity.kt
@@ -9,37 +9,16 @@ import androidx.camera.view.LifecycleCameraController
 import androidx.camera.view.PreviewView
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.material3.Button
-import androidx.compose.material3.Card
-import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
-import androidx.compose.ui.window.Dialog
 import com.empire.cameraxsample.ui.theme.CameraXSampleTheme
-import com.google.mlkit.vision.common.InputImage
-import com.google.mlkit.vision.text.TextRecognition
-import com.google.mlkit.vision.text.latin.TextRecognizerOptions
-import java.util.concurrent.Executors
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -68,45 +47,10 @@ fun CameraScreen() {
     cameraController.cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
     previewView.controller = cameraController
 
-    val executor = remember { Executors.newSingleThreadExecutor() }
-     var text by rememberSaveable {
-        mutableStateOf("")
-    }
-    var isLoading by remember { mutableStateOf(false) }
 
     Box(modifier = Modifier.fillMaxSize()) {
         AndroidView(factory = { previewView }, modifier = Modifier.fillMaxSize())
-        if (isLoading) {
-            CircularProgressIndicator(
-                modifier = Modifier
-                    .size(50.dp)
-                    .align(Alignment.Center)
-            )
-        } else {
-            IconButton(
-                modifier = Modifier
-                    .align(Alignment.BottomCenter)
-                    .padding(16.dp),
-                onClick = {
-                    isLoading = true
-                    cameraController.setImageAnalysisAnalyzer(executor) { imageProxy ->
-                        imageProxy.image?.let { image ->
-                            val img = InputImage.fromMediaImage(
-                                image,
-                                imageProxy.imageInfo.rotationDegrees
-                            )
-                        }
-                    }
-                }
-            ) {
-                Icon(
-                    painter = painterResource(id = R.drawable.baseline_camera_24),
-                    contentDescription = "",
-                    tint = MaterialTheme.colorScheme.primary,
-                    modifier = Modifier.size(54.dp)
-                )
-            }
-        }
+
     }
 }
 
