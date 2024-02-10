f0a8bb2603b68c97df0a447ec63ab541ef714dfb
Henok <henokmicrometre@gmail.com>
2024-02-10 19:45:46 +0000

    added camera

--- CameraXApp/app/src/main/java/com/example/cameraxapp/MainActivity.kt
+++ CameraXApp/app/src/main/java/com/example/cameraxapp/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.example.cameraxapp
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.camera.core.CameraSelector
+import androidx.camera.video.Recorder
+import androidx.camera.video.Recording
+import androidx.camera.video.VideoCapture
 import androidx.camera.view.LifecycleCameraController
 import androidx.camera.view.PreviewView
 import androidx.compose.foundation.layout.Box
@@ -15,6 +19,8 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
@@ -47,6 +53,9 @@ fun CameraScreen() {
     val previewView: PreviewView = remember { PreviewView(context) }
     val cameraController = remember { LifecycleCameraController(context) }
     val lifecycleOwner = LocalLifecycleOwner.current
+    var recording: Recording? = remember { null }
+    val videoCapture: MutableState<VideoCapture<Recorder>?> = remember { mutableStateOf(null) }
+    val recordingStarted: MutableState<Boolean> = remember { mutableStateOf(false) }
     cameraController.bindToLifecycle(lifecycleOwner)
     cameraController.cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
     previewView.controller = cameraController
@@ -54,7 +63,13 @@ fun CameraScreen() {
         AndroidView(factory = { previewView }, modifier = Modifier.fillMaxSize())
         IconButton(modifier = Modifier
             .padding(16.dp),
-            onClick = { /*TODO*/ }) {
+            onClick = {
+                Toast.makeText(
+                    context,
+                    "You clicked the button.",
+                    Toast.LENGTH_LONG)
+                    .show()
+            }) {
             Icon(
                 painter = painterResource(id = R.drawable.baseline_camera_24),
                 contentDescription = "",
