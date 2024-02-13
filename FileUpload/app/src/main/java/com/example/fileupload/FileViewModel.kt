d6a66639f3ca1a3ea08072d325db08a0f4902ce0
Henok <henokmicrometre@gmail.com>
2024-02-13 10:13:53 +0000

    added view

--- FileUpload/app/src/main/java/com/example/fileupload/FileViewModel.kt
+++ FileUpload/app/src/main/java/com/example/fileupload/FileViewModel.kt
@@ -1,11 +1,26 @@
 package com.example.fileupload
 
+import android.util.Log
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.fileupload.ui.theme.FileUploadTheme
+import com.example.fileupload.network.UserApi
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
+
+data class ProfileModel(
+    var name: String,
+)
+data class UserModel(
+    var profile: ProfileModel
+)
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
@@ -21,4 +36,26 @@ fun GreetingPreview() {
     FileUploadTheme {
         Greeting("Android")
     }
+}
+fun sendRequest(
+    id: String,
+    profileState: MutableState<ProfileModel>
+) {
+    val retrofit = Retrofit.Builder()
+        .baseUrl("http://192.168.1.122:3000")
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+    val api = retrofit.create(UserApi::class.java)
+    val call: Call<UserModel?>? = api.getUserById(id)
+    call!!.enqueue(object: Callback<UserModel?> {
+        override fun onResponse(call: Call<UserModel?>, response: Response<UserModel?>) {
+            if(response.isSuccessful) {
+                Log.d("Main", "success!" + response.body().toString())
+                profileState.value = response.body()!!.profile
+            }
+        }
+        override fun onFailure(call: Call<UserModel?>, t: Throwable) {
+            Log.e("Main", "Failed mate " + t.message.toString())
+        }
+    })
 }
--- FileUpload/app/src/main/java/com/example/fileupload/network/UserApi.kt
+++ FileUpload/app/src/main/java/com/example/fileupload/network/UserApi.kt
@@ -1,14 +1,16 @@
 package com.example.fileupload.network
 
+import com.example.fileupload.UserModel
 import retrofit2.http.GET
 import retrofit2.http.Headers
 import retrofit2.http.Path
+import retrofit2.Call
 
 public interface UserApi {
     @Headers(
         "Accept: application/json"
     )
     @GET("users/{id}")
-    abstract fun getUserById(@Path("id") id: String)
+    abstract fun getUserById(@Path("id") id: String): Call<UserModel?>?
 
 }
